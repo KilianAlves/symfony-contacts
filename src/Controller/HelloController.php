123a9fb73cb6242d0d9e880a5fa4dc770e734d5c
KilianAlves <alveskilian@gmail.com>
2022-10-10 17:41:11 +0200

    Test de l'action manytimes fonctionne grace a la magie noire

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -15,23 +15,25 @@ class HelloController extends AbstractController
             'controller_name' => 'HelloController',
         ]);
     }
+
     #[Route('/hello/{name}')]
     public function world(string $name): Response
     {
-        return $this->render('hello/world.html.twig',[
-            'name' => $name
+        return $this->render('hello/world.html.twig', [
+            'name' => $name,
         ]);
     }
 
-    #[Route('/hello/{name}/{times}', name: 'app_hello_manytimes')]
+    #[Route('/hello/{name}/{times}', name: 'app_hello_manytimes',requirements: ['times' => '\d+'],priority: 1)]
     public function manyTimes($name, int $times = 3)
     {
-        if ($times == 0 || $times > 10) {
-            return $this->redirectToRoute('app_hello_manytimes',['name' => $name, 'times' => 3]);
+        if (0 == $times || $times > 10) {
+            return $this->redirectToRoute('app_hello_manytimes', ['name' => $name,'times' => 3]);
         }
+
         return $this->render('hello/many_times.html.twig', [
             'name' => $name,
-            'times' => $times
+            'times' => $times,
         ]);
     }
 }
