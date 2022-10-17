c75d0b5bfc9a72ef77ec1521321ff0216642a24a
KilianAlves <alveskilian@gmail.com>
2022-10-17 17:53:00 +0200

    Fin 15.4

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use function PHPUnit\Framework\throwException;
 
 class ContactController extends AbstractController
 {
@@ -16,5 +18,17 @@ class ContactController extends AbstractController
         return $this->render('contact/index.html.twig', [
             'contacts' => $contacts
         ]);
+    }
+
+    #[Route('/contact/{id}', name: 'app_contact_show', requirements: ['contactId' => '\d+'])]
+    public function show(ContactRepository $contact, $id): Response
+    {
+        $Usercontact = $contact->findOneById($id);
+        // $Usercontact = $contact->findOneBy(['id' => $id]);
+
+        if ($Usercontact == null){
+            throw new NotFoundHttpException("L'id n'est pas valide");
+        }
+        return $this->render('contact/show.html.twig', ['contact' => $Usercontact]);
     }
 }
