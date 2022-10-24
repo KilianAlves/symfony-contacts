2f598f2cbc64666df6145076d8a00fd519cec8ad
KilianAlves <alveskilian@gmail.com>
2022-10-24 17:11:39 +0200

    Fin 16.6

--- tests/Controller/Contact/IndexCest.php
+++ tests/Controller/Contact/IndexCest.php
@@ -3,25 +3,31 @@
 
 namespace App\Tests\Controller\Contact;
 
+use App\Factory\ContactFactory;
 use App\Tests\Support\ControllerTester;
+use function Zenstruck\Foundry\create_many;
 
 class IndexCest
 {
     public function index(ControllerTester $I): void
     {
+        ContactFactory::createMany(5);
+
         $I->amOnPage("/contact");
         $I->seeResponseCodeIsSuccessful();
         $I->seeInTitle('Liste des contacts');
         $I->see('Liste des contacts', 'h1');
-        $I->seeNumberOfElements('li', 195);
-        $I->seeNumberOfElements('a', 195);
+        $I->seeNumberOfElements('li', 5);
+        $I->seeNumberOfElements('a', 5);
     }
 
     public function show(ControllerTester $I): void
     {
+        ContactFactory::createOne(['firstname' => 'Joe', 'lastname' => 'Aaaaaaaaaaaaaaa']);
         $I->amOnPage("/contact/1");
         $I->seeResponseCodeIsSuccessful();
-        $I->seeInTitle('Jacquot Richard');
-        $I->see('Jacquot Richard','h1');
+        $I->seeInTitle('Aaaaaaaaaaaaaaa Joe');
+        $I->see('Aaaaaaaaaaaaaaa Joe','h1');
+        $I->seeCurrentRouteIs('app_contact_show', ['id' => 1]);
     }
 }
