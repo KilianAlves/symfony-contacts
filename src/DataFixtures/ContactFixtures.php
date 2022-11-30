aa96123274f201fb5c86ef64b1be4ef3f868cf5f
KilianAlves <alveskilian@gmail.com>
2022-11-30 17:20:26 +0100

    Fin du 19.4.11

--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategoryFactory;
 use App\Factory\ContactFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use function Zenstruck\Foundry\faker;
 
-class ContactFixtures extends Fixture
+class ContactFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        ContactFactory::createMany(150);
+        ContactFactory::createMany(150, function() {
+            if (faker()->boolean(90)) {
+                return [
+                    'category' => CategoryFactory::random(),
+                ];
+            } else {
+                return [
+                ];
+            }
+        });
+        //ContactFactory::createOne(['category' => CategoryFactory::random()]);
+//
     }
 
     public function getDependencies()
