2744d51da046f36845fbe08d0faae71549401ae0
KilianAlves <alveskilian@gmail.com>
2022-11-30 18:26:48 +0100

    Fin de scéance

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
 
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    public function index(categoryRepository $categoryRepository): Response
     {
+        $categories = $categoryRepository->findBy([],['name' => 'ASC']);
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories,
         ]);
     }
 }
